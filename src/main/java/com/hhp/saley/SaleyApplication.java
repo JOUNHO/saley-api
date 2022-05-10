6c03326d4bccb0b338447c456b4678832eff339c
JOUNHO <dnsgh9177@naver.com>
2022-05-10 23:04:20 +0900

    saley-api 최초 커밋

---
+++ src/main/java/com/hhp/saley/SaleyApplication.java
@@ -0,0 +1,13 @@
+package com.hhp.saley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaleyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hhp/saley/SaleyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhp.saley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
